81f6e871e7b4e685d70b8f616bf14aa0877c2e0d
shalu <shalu.s2021@gmail.com>
2023-09-05 13:51:03 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <div>
+            <h1>TechyStar</h1>
+            <p>@all rights reserved.</p>
+        </div>
+
+        <div>
+            <h5>Follow Us</h5>
+             <div>
+                <a href="/" target={"blank"}>
+                    LinkedIn
+                </a>
+                <a href="/" target={"blank"}>
+                    GitHub
+                </a>
+            </div> 
+        </div>
+
+    </footer>
+  )
+}
+
+export default Footer
